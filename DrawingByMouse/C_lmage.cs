e7dc8e1ff30d0783bfd158c6b9f7503e9a0a972c
Miguel Benalcazar <mabhvebh@gmail.com>
2018-11-29 23:39:37 +0800

    Add project files.

---
+++ DrawingByMouse/C_lmage.cs
@@ -0,0 +1,127 @@
+﻿using OpenCvSharp;
+using System.Drawing;
+
+
+namespace DrawingByMouse
+{
+    class C_lmage
+    {
+        //Open Image
+        public static Mat C_Image_OpenImage(string path, int type)
+        {
+            Mat Image = new Mat();
+            switch (type)
+            {
+                case 1:                                                             //Case 1 return color
+                    Image = new Mat(path, ImreadModes.Color);
+                    break;  
+                case 2:                                                             //Case 2 return GrayScale
+                     Image = new Mat(path, ImreadModes.GrayScale);
+                    break;
+            }
+            return Image;
+        }
+        //convert image into gray values
+
+        public static Mat C_Image_GrayValue(Mat image)
+        {
+            Mat image_copy = new Mat();
+            Cv2.CvtColor(image, image_copy, ColorConversionCodes.BGR2GRAY);
+            return image_copy;
+        }
+        //Normalize
+        public static Mat C_Image_Normalize(Mat image)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Normalize(image,image_copy,0,255,NormTypes.MinMax,-1,null);
+            return image_copy;
+        }
+        //Thresholding
+        public static Mat C_Image_Thresholding(Mat image, double thres)
+        {
+            Mat image_copy = new Mat();
+          
+            Cv2.Threshold(image, image_copy, thres, 255, ThresholdTypes.Binary);
+            Cv2.Threshold(image_copy, image_copy, thres, 255, ThresholdTypes.Otsu);
+            return image_copy;
+        }
+        //Mirror
+        public static Mat C_Image_Mirror(Mat image)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Flip(image, image_copy, FlipMode.X);
+            return image_copy;
+        }
+        //FLIP
+        public static Mat C_Image_Flip(Mat image)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Flip(image, image_copy, FlipMode.Y);
+            return image_copy;
+        }
+
+        //Zoom
+        public static Mat C_Image_Resize(Mat image,int width,int height)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Resize(image, image_copy, new OpenCvSharp.Size(width, height), 0, 0, InterpolationFlags.Lanczos4);
+            return image_copy;
+        }
+        public static Mat C_Image_Resize(Mat image,double factor)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Resize(image, image_copy, OpenCvSharp.Size.Zero, factor, factor, InterpolationFlags.Lanczos4);
+            return image_copy;
+        }
+
+        //Gaussian filter .. blur
+        public static Mat C_Image_Gaussian(Mat image,int size_x,int size_y,double sigma)
+        {
+            Mat image_copy = new Mat();
+            Cv2.GaussianBlur(image, image_copy, new OpenCvSharp.Size(size_x, size_y), sigma,0,BorderTypes.Replicate);
+            return image_copy;
+        }
+        
+        //Sobel filter .. 
+        public static Mat C_Image_Sobel(Mat image, int x_order, int y_order)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Sobel(image,image_copy, -1,x_order,y_order,3,1,0,BorderTypes.Default);
+            return image_copy;
+        }
+        // https://docs.opencv.org/2.4/modules/imgproc/doc/filtering.html?highlight=sobel#sobel
+
+        //Canny filter
+        public static Mat C_Image_Canny(Mat image,double thres1)
+        {
+            Mat image_copy = new Mat();
+            Cv2.Canny(image, image_copy, thres1, thres1, 3, true);
+            //Cv2.Canny(image, image_copy, thres1, thres1*2,3,true);   
+            return image_copy;
+        }
+        //Draw contours
+        public static Mat C_Image_Draw_Contours(Mat image, Mat Origi_Image)
+        {
+            OpenCvSharp.Point[][] contours_data;
+            HierarchyIndex[] hierarchyIndexes;
+            Cv2.FindContours(image, out contours_data, out hierarchyIndexes,RetrievalModes.Tree,ContourApproximationModes.ApproxSimple);
+            for(int i = 0; i < contours_data.Length; i++)
+            {
+                Cv2.DrawContours(Origi_Image, contours_data, i, Scalar.Red, 2, LineTypes.Link8, hierarchyIndexes);
+            }
+            return Origi_Image;
+        }
+         //Convert from Mat to Bitmap
+        public static Bitmap MatToBitmap(Mat image)
+        {
+            return OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image);
+        }
+        //Convert from Bitmap to Mat
+        public static Mat BitmapToMat(Bitmap image)
+        {
+            return OpenCvSharp.Extensions.BitmapConverter.ToMat(image);
+        }
+        
+
+    }
+}
---
+++ DrawingByMouse/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace DrawingByMouse
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Color color = new System.Drawing.Color();
+        int pen_size = new int();
+        float x1, x2, y1, y2;
+
+        System.Drawing.Point mDown = System.Drawing.Point.Empty;
+        System.Drawing.Point mCurrent = System.Drawing.Point.Empty;
+        Bitmap bitmap;
+        Bitmap a,m,empty;
+        Pen pen = new Pen(Brushes.Red);
+        Graphics g;
+        C_lmage c_img = new C_lmage();
+        Boolean down = new Boolean();
+        Mat img_;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //mDown = System.Drawing.Point.Empty;
+            down = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mDown = e.Location;
+           mDown = TranslateStretchImageMousePosition(mDown);
+            down = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            long bytes1 = GC.GetTotalMemory(false);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left && down)
+            {
+                
+                pen.Width = pen_size;
+                pen.Color = color;
+                //set the linejoin (not needed, but gives better results)
+                pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+
+                using (g = Graphics.FromImage(bitmap))
+                {
+                     
+                    Graphics dd = Graphics.FromImage(empty);
+                
+                    g.DrawImage(a, 0, 0);
+                    mCurrent = e.Location;
+                    mCurrent = TranslateStretchImageMousePosition(mCurrent);
+                    g.DrawLine(pen, mDown, mCurrent);//
+                    
+                    dd.DrawImage(m, 0, 0);
+                    dd.DrawLine(pen, mDown, mCurrent);
+                    
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                 
+                }
+
+
+
+                m = empty;
+                pictureBox1.Invalidate();
+                a = bitmap;
+                pictureBox1.Image = bitmap;
+
+
+                mDown = e.Location;
+               mDown = TranslateStretchImageMousePosition(mDown);
+
+            }
+            if (e.Button == System.Windows.Forms.MouseButtons.Right && down)
+            {
+                mCurrent = e.Location;
+                mCurrent = TranslateStretchImageMousePosition(mCurrent);
+                pen.Width = 2;
+                //set the linejoin (not needed, but gives better results)
+                pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+
+                using (g = Graphics.FromImage(bitmap))
+                {
+
+
+                    g.Clear(Color.Red);
+                }
+
+
+
+
+                pictureBox1.Invalidate();
+                a = bitmap;
+                pictureBox1.Image = bitmap;
+
+
+                mDown = e.Location;
+
+            }
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            //if (mDown != Point.Empty)
+            //    e.Graphics.DrawLine(Pens.White, mDown, mCurrent);
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+
+            {
+                pictureBox1.Image = null;
+
+                Invalidate();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Mat aa = C_lmage.BitmapToMat(bitmap);
+            Mat bb= C_lmage.BitmapToMat(empty);
+            aa = C_lmage.C_Image_Resize(aa, img_.Width, img_.Height);
+            Cv2.ImWrite("C:\\Users\\Malky_PC\\Pictures\\test\\test.jpg", aa);
+            Cv2.ImWrite("C:\\Users\\Malky_PC\\Pictures\\test\\test_.png", bb);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+           if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color = colorDialog1.Color;
+                Console.WriteLine("color -> " + color.ToString());
+            }
+            
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            pen_size = int.Parse(textBox1.Text);     
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "bmp files (*.bmp)|*.bmp";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+
+                    img_= C_lmage.C_Image_OpenImage(dlg.FileName,1);
+                    //Mat img = C_lmage.C_Image_Resize(img_, pictureBox1.Width, pictureBox1.Height);
+                    Bitmap gg = C_lmage.MatToBitmap(img_);
+                    pictureBox1.Image = gg;
+                    a = new Bitmap(gg);//bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    bitmap = new Bitmap(a.Width, a.Height);
+                    m = new Bitmap(img_.Width, img_.Height);
+                    empty = new Bitmap(img_.Width, img_.Height);
+                    //bitmap = new Bitmap(dlg.FileName);
+                }
+            }
+        }
+
+        protected System.Drawing.Point TranslateStretchImageMousePosition(System.Drawing.Point coordinates)
+        {
+            float x = (float)(pictureBox1.Image.Width / pictureBox1.ClientSize.Width);
+            float y = (float)(pictureBox1.Image.Height / pictureBox1.ClientSize.Height);
+
+            Console.WriteLine(" pictureBox image width -> " + pictureBox1.Image.Width.ToString() 
+                + " pictureBox image client -> " + pictureBox1.ClientSize.Width.ToString()+" "+x.ToString());
+            Console.WriteLine(" pictureBox image height -> " + pictureBox1.Image.Height.ToString()
+                + " pictureBox image client -> " + pictureBox1.ClientSize.Height.ToString() + " " + y.ToString());
+
+            return new System.Drawing.Point((int)(coordinates.X*x), (int)(coordinates.Y*y));
+
+            //// test to make sure our image is not null
+            //if (img_ == null) return coordinates;
+            //// Make sure our control width and height are not 0
+            //if (Width == 0 || Height == 0) return coordinates;
+            //// First, get the ratio (image to control) the height and width
+            //float ratioWidth = (float)img_.Width / Width;
+            //float ratioHeight = (float)img_.Height / Height;
+            //// Scale the points by our ratio
+            //float newX = coordinates.X;
+            //float newY = coordinates.Y;
+            //newX *= ratioWidth;
+            //newY *= ratioHeight;
+            //return new System.Drawing.Point((int)newX, (int)newY);
+        }
+        protected float[] Position(System.Drawing.Point coordinates)
+        {
+            float[] coordinates_ = new float[2];
+            coordinates_[0] = (float)pictureBox1.Image.Width / (float)pictureBox1.ClientSize.Width;  //x
+            coordinates_[1] = (float)pictureBox1.Image.Height / (float)pictureBox1.ClientSize.Height; //y
+
+            Console.WriteLine(" pictureBox image width -> " + pictureBox1.Image.Width.ToString()
+                + " pictureBox image client -> " + pictureBox1.ClientSize.Width.ToString() + " " + coordinates_[0]);
+            Console.WriteLine(" pictureBox image height -> " + pictureBox1.Image.Height.ToString()
+                + " pictureBox image client -> " + pictureBox1.ClientSize.Height.ToString() + " " + coordinates_[1].ToString());
+
+            return coordinates_;
+
+            //// test to make sure our image is not null
+            //if (img_ == null) return coordinates;
+            //// Make sure our control width and height are not 0
+            //if (Width == 0 || Height == 0) return coordinates;
+            //// First, get the ratio (image to control) the height and width
+            //float ratioWidth = (float)img_.Width / Width;
+            //float ratioHeight = (float)img_.Height / Height;
+            //// Scale the points by our ratio
+            //float newX = coordinates.X;
+            //float newY = coordinates.Y;
+            //newX *= ratioWidth;
+            //newY *= ratioHeight;
+            //return new System.Drawing.Point((int)newX, (int)newY);
+        }
+    }
+}
